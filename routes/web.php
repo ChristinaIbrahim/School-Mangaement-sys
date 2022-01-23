38d1a0b64485a808dc7873df5e4fe636c076a14f
Christina Ibrahim <christina.ibrahim.aziz@gmail.com>
2022-01-23 12:13:59 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('auth.login');
 });
+Auth::routes();
+
+// Route::get('/dashboard', function () {
+// return view('dashboard');
+// });
+
+Route::get('/dashboard', 'HomeController@index')->name('dashboard');
